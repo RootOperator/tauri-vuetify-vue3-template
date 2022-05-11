6e1065c3b60cf113f90f8d19ea7d4681b6819afe
RootOperator <20143055+RootOperator@users.noreply.github.com>
2022-05-11 10:32:37 +0200

    template code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+        <v-navigation-drawer app>
+        </v-navigation-drawer>
+
+        <v-app-bar app>
+        </v-app-bar>
+        <v-main>
+            <v-container fluid>
+                <HelloWorld msg="Tauri app with Vuetify"></HelloWorld>
+                <v-btn elevation="2" rounded @click="test">Vuetify button {{ count }}</v-btn>
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
@@ -10,6 +24,16 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+      return {
+          count: 0
+      }
+  },
+  methods: {
+    test() {
+        this.count = 5
+    }
   }
 }
 </script>
